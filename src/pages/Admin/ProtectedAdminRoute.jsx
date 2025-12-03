8f81ce674791f995f67d0017469a27b0dc41f75a
ashfak88 <ashfakkp323@gmail.com>
2025-12-03 14:21:05 +0530

    user login check
    Removed comment about user not logged in.

--- src/pages/Admin/ProtectedAdminRoute.jsx
+++ src/pages/Admin/ProtectedAdminRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 const ProtectedAdminRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
-  // ✅ User not logged in → go to login page
   if (!user) {
     return <Navigate to="/login" replace />;
   }
